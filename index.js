3b2731396055985d3cbde89f187efe74c4afc1fa
zain1234555 <zainq979@gmail.com>
2024-05-18 01:05:55 +0500

    CALCULATOR CLI

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /user/bin/env node
+import inquirer from 'inquirer';
+// Printing A Wellcome Message
+console.log("\n\tWellcome to \'qureshi44\' - CLI Simpale Calculator\n");
+// Asking Qustions From Usere Through inquirer
+const answer = await inquirer.prompt([
+    { Message: "enter your firstnumbar", type: "numbar", name: "firstnumbar" },
+    {
+        Message: "enter your secendnumbar",
+        type: "numbar",
+        name: "secendnumbar",
+    },
+    {
+        message: "select one of opreter to perfom action",
+        type: "list",
+        name: "opreter",
+        choices: ["ADDITION", "SUBTRAION", "MULTIPALCTION", "DIVISON"],
+    },
+]);
+// candition statment
+if (answer.opreter === "ADDITION") {
+    console.log(answer.firstnumbar + answer.secendnumbar);
+}
+else if (answer.opreter === "SUBTRAION") {
+    console.log(answer.firstnumbar - answer.secendnumbar);
+}
+else if (answer.opreter === "MULTIPALCTION") {
+    console.log(answer.firstnumbar * answer.secendnumbar);
+}
+else if (answer.opreter === "DIVISON") {
+    console.log(answer.firstnumbar / answer.secendnumbar);
+}
+else {
+    "plz select correct opreter";
+}
+;
